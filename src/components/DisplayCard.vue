9ccdfdac15aa5b8421cd6efa6b3947ac7605fd38
Andrea Prina <and.prina@gmail.com>
2022-07-21 17:20:58 +0200

    feat(DisplayCard): Create card component to display movies and shows

---
+++ src/components/DisplayCard.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <img :src="posterSource" :alt="title">
+        <h4>{{title}}</h4>
+        <h5>{{originalTitle}}</h5>
+        <img class="language-flag" :src="flagSource" :alt="originalLanguage">
+        <p>{{averageVote}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+
+    props : {
+        
+        posterSource : String,
+        title : String,
+        originalTitle : String,
+        originalLangue : String,
+        flagSource : String,
+        averageVote : Number,
+
+    }
+
+}
+</script>
+
+<style>
+
+</style>
