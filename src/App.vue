f793637242443ca99f1eb0044c97a5297ac2d531
Andrea Prina <and.prina@gmail.com>
2022-07-21 13:00:08 +0200

    build:

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-
+  
   </div>
 </template>
 
@@ -14,6 +14,8 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+
+  @import '~bootstrap/scss/bootstrap';
 
 </style>
