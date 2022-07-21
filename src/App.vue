56287d1799435c792146ca4d8267ae4f8b8b5774
Andrea Prina <and.prina@gmail.com>
2022-07-21 13:26:45 +0200

    feat(SearchBar): Create search bar component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
-  
+    <SearchBar class="w-50"/>
   </div>
 </template>
 
 <script>
+import SearchBar from './components/SearchBar.vue'
+
 import axios from 'axios';
 
 
 export default {
   name: 'App',
   components: {
+    SearchBar,
   }
 }
 </script>
@@ -18,5 +21,11 @@ export default {
 <style lang="scss">
 
   @import '~bootstrap/scss/bootstrap';
+
+  #app{
+    background-color: black;
+    color: white;
+    height: 100vh;
+  }
 
 </style>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <input type="text" placeholder="Search movie or TV series">
+        <button>Search</button>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+    input[type='text']{
+        width: 85%;
+    }
+
+    button {
+        width: 15%;
+    }
+
+</style>
