3000f992b377ceb93f2801252242d31d872b4197
Andrea Prina <and.prina@gmail.com>
2022-07-21 15:07:10 +0200

    feat(DisplayList): Add axios API call to TMDB with static query
    Set up the API call to Search Movie API of The Movie DB the information to be displayed in the main section of the app

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <SearchBar class="w-50" @search="log"/>
+    <div>{{moviesTitle[1].title}}</div>
   </div>
 </template>
 
@@ -16,10 +17,28 @@ export default {
     SearchBar,
   },
 
+  data : function(){
+    return {
+      moviesTitle : [],
+    }
+  },
+
   methods : {
     log : function(string){
       console.log(string);
+    },
+
+    getMovies : function(query){
+      axios.get(`https://api.themoviedb.org/3/search/movie?api_key=1f3169b87aab636f5fde0cfa52d8788d&query=${query}`)
+      .then((result) => {
+        this.moviesTitle = result.data.results;
+        console.log(result.data.results[1].title);
+      })
     }
+  },
+
+  created(){
+    this.getMovies('back');
   }
 }
 </script>
